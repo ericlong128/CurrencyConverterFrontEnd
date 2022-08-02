c0209e491c6ea206e4e80bd4c0cf412138e954c8
Vichet Meng <vichet.meng01@ad.infosys.com>
2022-08-02 16:55:22 -0500

    added customer model class

---
+++ bank-service-portal/src/shared/models/customer.ts
@@ -0,0 +1,14 @@
+
+export class Customer {
+    // private String emailId;
+    // private String name;
+    // private String password;
+    // private String phoneNumber;
+    // private List<Address> addresses;
+    emailId: string;
+    name: string;
+    password: string;
+    newPassword: string;
+    phoneNumber: string;
+    //check for customer carts
+}
