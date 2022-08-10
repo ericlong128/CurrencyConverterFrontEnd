b032af8fb8a90d5a6062de4861bebf1fbd50afe9
Flobert Wangsawihardja <flobert.w@ad.infosys.com>
2022-08-10 12:08:07 -0500

    Login functionalities and imports. Controller endpoints added?

--- bank-service-portal/src/app/services/login.service.ts
+++ bank-service-portal/src/app/services/login.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/User';
+import { Router } from '@angular/router';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +10,13 @@ export class LoginService {
 
   constructor(private httpCli : HttpClient, private router : Router) { }
 
+  user : User = <User>{};
+  username : string = "";
+  password: string = "";
+
+
   login(username : string, password : string) {
-    return this.httpCli.post<any>(`localhost:4200/session`, {
+    return this.httpCli.post<any>(`localhost:4200/api/customers`, {
       "username" : username,
       "password" : password}, {
       withCredentials : true
@@ -16,13 +24,13 @@ export class LoginService {
   }
 
   checkSession() {
-    return this.httpCli.get<any>(`localhost:4200/session`, {
+    return this.httpCli.get<any>(`localhost:4200/api/customers/${this.username}`, {
       withCredentials : true
     });
   }
 
   logout() {
-    return this.httpCli.delete<any>(`localhost:4200/session`, {
+    return this.httpCli.delete<any>(`localhost:4200/api/customers/${this.username}`, {
       withCredentials : true
     });
   }
